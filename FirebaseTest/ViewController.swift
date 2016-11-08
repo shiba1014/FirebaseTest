40869c1c2b94ee830b6e59ce908bbc32d1fbbb72
shiba1014 <shiba.png@gmail.com>
2016-11-08 15:34:07 +0900

    Initial Commit

---
+++ FirebaseTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseTest
+//
+//  Created by Paul McCartney on 2016/11/08.
+//  Copyright © 2016年 shiba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
